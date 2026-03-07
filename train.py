df88ad085607ee22e196e3511cfda5fae3c20c22
Neelkanth Shitolay <109946481+neelkanth-s@users.noreply.github.com>
2026-03-06 16:36:56 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,37 +1,45 @@
-from keras.datasets import fashion_mnist
-import tensorflow
-import matplotlib.pyplot as plt
-import numpy as np
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+#from keras.datasets import fashion_mnist
+#import tensorflow
+#import matplotlib.pyplot as plt
+#import numpy as np
+#(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-print(x_train.shape)   # (60000, 28, 28)
-print(y_train.shape)   # (60000,)
+#print(x_train.shape)   # (60000, 28, 28)
+#print(y_train.shape)   # (60000,)
 
-x_class = x_train[y_train == 0]
-fig, axes = plt.subplots(1, 5, figsize=(10,2))
+#x_class = x_train[y_train == 0]
+#fig, axes = plt.subplots(1, 5, figsize=(10,2))
 
-for i, ax in enumerate(axes):
-    ax.imshow(x_class[i], cmap="gray")
-    ax.axis("off")
+#for i, ax in enumerate(axes):
+#    ax.imshow(x_class[i], cmap="gray")
+#    ax.axis("off")
 
-plt.show()
+#plt.show()
 
-X_train_flat = x_class.reshape(len(x_class), 28*28)
-print(np.shape(X_train_flat))
-mu = np.mean(X_train_flat,0)
-var = np.var(X_train_flat,0)
-mu = mu.reshape(28,28)
-plt.imshow(mu, cmap="gray")
-plt.show()
+#X_train_flat = x_class.reshape(len(x_class), 28*28)
+#print(np.shape(X_train_flat))
+#mu = np.mean(X_train_flat,0)
+#var = np.var(X_train_flat,0)
+#mu = mu.reshape(28,28)
+#plt.imshow(mu, cmap="gray")
+#plt.show()
+
+import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from model import VAE
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Training hyperparameters
 batch_size = 128
 num_epochs = 20
 learning_rate = 1e-3
 
 transform = transforms.ToTensor()
 
+# Dataset loading (Using FashionMNIST as our dataset)
 train_dataset = datasets.FashionMNIST(
     root="./data",
     train=True,
@@ -41,24 +49,32 @@ train_dataset = datasets.FashionMNIST(
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
+
+# Model initialization, Using Adam to optimze parameters
 model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 
+#VAE loss function
 def vae_loss(recon_x, x, mu, logvar):
+
+    #Reconstruction Loss (From section 2 and 3)
     recon_loss = torch.nn.functional.binary_cross_entropy(
         recon_x,
         x,
         reduction="sum"
     )
 
+    #KL divergence 
     kl_loss = -0.5 * torch.sum(
         1 + logvar - mu.pow(2) - logvar.exp()
     )
 
+    #Total Loss
     return recon_loss + kl_loss
 
 
+#Training Loop
 for epoch in range(num_epochs):
 
     epoch_loss = 0
@@ -72,6 +88,9 @@ for epoch in range(num_epochs):
 
         loss.backward()
         optimizer.step()
+
+        #forward parametrization 
+        #loss.forward()
 
         epoch_loss += loss.item()
 
