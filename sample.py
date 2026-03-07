e1dad8901bf5ba002855ca19e6c92d95c3970f2a
Neelkanth Shitolay <109946481+neelkanth-s@users.noreply.github.com>
2026-03-06 16:35:31 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,14 +4,18 @@ from model import VAE
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Load trained model
 model = VAE().to(device)
 model.load_state_dict(torch.load("vae_fashion.pth"))
 model.eval()
 
+# Generate new samples
 with torch.no_grad():
 
+    # Sample latent variables (sampling 16 vectors from the dataset)
     z = torch.randn(16, 20).to(device)
 
+    # Decode latent variables into images
     generated = model.decoder(z)
     generated = generated.view(-1, 28, 28)
 
@@ -22,7 +26,7 @@ for i in range(16):
     row = i // 4
     col = i % 4
 
-    axes[row, col].imshow(generated[i].cpu())
+    axes[row, col].imshow(generated[i].cpu(), cmap="gray")
     axes[row, col].axis("off")
 
 plt.show()
